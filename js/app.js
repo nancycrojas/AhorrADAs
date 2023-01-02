290132ca87e681fb4598374d4dd56b312c458c9e
nancycrojas <nanclarisaok@gmail.com>
2023-01-02 15:50:57 -0300

    Add functionality for screen flow

--- js/app.js
+++ js/app.js
@@ -4,8 +4,16 @@ const $ = (selector) => document.querySelector(selector);
 //Variables
 const $navbarMenu = $(".navbar-menu");
 const $btnBurger = $(".navbar-burger");
-const $formFilters =$("#form-filters");
-const $btnChangeFilters =$("#btn-change-filters");
+const $formFilters = $("#form-filters");
+const $btnChangeFilters = $("#btn-change-filters");
+const $btnBalance = $("#btn-balance");
+const $btnCategories = $("#btn-categories");
+const $btnReports = $("#btn-reports");
+const $sectionBalance = $("#section-balance");
+const $sectionCategories = $("#section-categories");
+const $sectionReports = $("#section-reports");
+const $sectionNewOperation = $("#section-new-operation");
+const $btnNewOperation = $("#btn-new-operation")
 
 //Eventos
 //Menú hamburguesa
@@ -22,4 +30,34 @@ $btnChangeFilters.addEventListener("click",()=>{
     }else{
         $btnChangeFilters.textContent="Ocultar filtros";
     }
+});
+
+//Flujo de pantallas
+$btnBalance.addEventListener("click",()=>{
+    $sectionBalance.classList.remove("is-hidden")
+    $sectionCategories.classList.add("is-hidden")
+    $sectionReports.classList.add("is-hidden")
+    $sectionNewOperation.classList.add("is-hidden")
+}); 
+
+
+$btnCategories.addEventListener("click",()=>{
+    $sectionCategories.classList.remove("is-hidden")
+    $sectionBalance.classList.add("is-hidden")
+    $sectionReports.classList.add("is-hidden")
+    $sectionNewOperation.classList.add("is-hidden")
+});
+
+$btnReports.addEventListener("click",()=>{
+    $sectionReports.classList.remove("is-hidden")
+    $sectionBalance.classList.add("is-hidden")
+    $sectionCategories.classList.add("is-hidden")
+    $sectionNewOperation.classList.add("is-hidden")
+});
+
+$btnNewOperation.addEventListener("click",()=>{
+    $sectionNewOperation.classList.remove("is-hidden")
+    $sectionBalance.classList.add("is-hidden")
+    $sectionCategories.classList.add("is-hidden")
+    $sectionReports.classList.add("is-hidden")
 });
